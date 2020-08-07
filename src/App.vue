f18018ec2c446806622b656076c9cc024c69d2b6
Luis Lucas <luislucas@pop-os.localdomain>
2020-08-06 21:23:04 -0300

    Task.vue init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<h1>To do list</h1>
+		<TaskGrid :tasks="tasks"/>
+	</div>
+</template>
+
+<script>
+import TaskGrid  from './components/TaskGrid';
+
+export default {
+	components: { TaskGrid },
+	data () {
+		return {
+			tasks: [
+				{ name: 'Lavar roupa', pending: false},
+				{ name: 'Comprar blusa', pending: true}
+			]
+		}
+	}
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Roboto', sans-serif;
+		background: black;
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="task"
+        :class="stateClass">
+      <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: { type: Object, required: true}
+    },
+    computed: {
+        stateClass() {
+            return {
+                pending: this.task.pending,
+                done: !this.task.pending
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.task {
+    box-sizing: border-box;
+    width: 150px;
+    height:  150px;
+    padding: 10px;
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 300;
+    cursor: pointer;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.pending {
+    border-left: 12px solid rgb(255, 136, 0);
+    background-color: orange;
+}
+
+.done {
+    color: rgba(255, 255, 255, 0.384);
+    border-left: 12px solid rgb(0, 128, 64);
+    background-color: green;
+    text-decoration: line-through;
+}
+</style>
